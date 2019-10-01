12aa02dae2613ca6d659185ed2a23fec38e55be7
Gersian-Martinez <martinezgersain30@gmail.com>
2019-10-01 18:16:01 -0500

    Initial commit

---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p1gmhpresentacion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmh.itics.tesoem.edu.p1gmhpresentacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
